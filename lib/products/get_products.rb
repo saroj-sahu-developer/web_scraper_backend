6b32717db747befad257479e1518d57d25874d8b
Saroj-Sahu <sahusarojkumar2020@gmail.com>
2025-02-18 15:37:44 +0530

    Create a generic library to fetch products

---
+++ lib/products/get_products.rb
@@ -0,0 +1,38 @@
+module Products
+  class GetProducts
+    class << self
+      def get(
+          order_by: default_order_by, 
+          order: default_order, 
+          pagination: true, 
+          page: default_page, 
+          per_page: default_limit
+        )
+
+        if pagination
+          products = Product.order(order_by => order).page(page).per(per_page)
+        else
+          products = Product.order(order_by => order)
+        end 
+
+        products
+      end
+
+      def default_order_by
+        'id'
+      end
+
+      def default_order
+        'asc'
+      end
+
+      def default_limit
+        10
+      end
+
+      def default_page
+        1
+      end
+    end
+  end
+end
