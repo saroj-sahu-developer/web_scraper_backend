c13829a553b2485e287f5e1f204578884414a221
Saroj-Sahu <sahusarojkumar2020@gmail.com>
2025-02-19 14:10:53 +0530

    Add a small refactor

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  has_many :products
+  has_many :products, dependent: :nullify
 
   validates :title, presence: true, uniqueness: true
   # As category being fetched from webscraper, if a category already exists in db, then the product should belong so that category so uniqueness: true
